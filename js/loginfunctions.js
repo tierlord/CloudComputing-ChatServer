2e075058600ab5a52d9c68e3f97c78de4386d23f
Joffrey <>
2018-10-10 12:22:03 +0200

    Minor fixes

---
+++ js/loginfunctions.js
@@ -0,0 +1,35 @@
+$('#form').submit(function(e){
+    e.preventDefault();
+    var t = document.getElementById("textfield").value;
+    console.log("Submit "+ t);
+    if(t == ''){
+        redTextfield(true);
+        return false;
+    }
+    // Check if username is free
+    $.get("user/" + t, function(data, status){
+        if(data == 'used'){
+            alert("Sorry, username is already taken.");
+        }
+        if(data == 'free'){
+            // login with username					
+            location.href = "/chat/" + t;					
+        }
+    });			
+    return false;
+});
+
+$('#textfield').on('input', function(){
+    var t = document.getElementById("textfield").value;
+    if(t.indexOf('/') != -1 || t.indexOf(' ') != -1){
+        redTextfield(true);
+        console.log("Error");
+    } else {
+        redTextfield(false);
+    }
+});
+
+function redTextfield(enabled){
+    if(enabled) $('#textfield').css('borderBottom', '1px solid #f00');
+    else $('#textfield').css('borderBottom', '1px solid #fff');
+}
