724fe2183f66d0a4f635f4924b2d73c961a19435
Joffrey Schneider <joffrey.schneider@student.reutlingen-university.de>
2018-11-22 21:32:51 +0100

    ibm_db

--- node_modules/ibm_db/test/promise-test-open-close.js
+++ node_modules/ibm_db/test/promise-test-open-close.js
@@ -1,34 +1,26 @@
 var common = require("./common")
-  , ibmdb = require("../")
-  , db = new ibmdb.Database()
+  , odbc = require("../")
+  , db = new odbc.Database()
   , assert = require("assert");
+
+//assert.equal(db.connected, false);
 
 db.query("select * from " + common.tableName).then(null, function (err) {
   assert.deepEqual(err, { message: 'Connection not open.' });
   assert.equal(db.connected, false);
 });
 
-db.open(common.connectionString).then(function(res) {
-  console.log("db = ", db);
-  assert.equal(db.connected, true);
-  db.query("select * from " + common.tableName).then(null, function (err) {
-	var errorFound = false;
-	if(err) console.log(err);
-	if (err.message) {
-	    var errorFound = err.message.includes("SQL0204N");
-	}
-    assert.equal(errorFound, true);
-    assert.equal(db.connected, true);
-    db.close().then(function () {
-      assert.equal(db.connected, false);
 
-      db.query("select * from " + common.tableName).then(null, function (err) {
-	    if(err) console.log(err);
-        assert.deepEqual(err, { message: 'Connection not open.' });
-        assert.equal(db.connected, false);
-      });
+
+db.open(common.connectionString).then(function(res) {
+  assert.equal(db.connected, true);
+
+  db.close().then(function () {
+    assert.equal(db.connected, false);
+
+    db.query("select * from " + common.tableName).then(null, function (err) {
+      assert.deepEqual(err, { message: 'Connection not open.' });
+      assert.equal(db.connected, false);
     });
   });
-})
-.catch(function(err){console.log(err);})
-.done();
+});
