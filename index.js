542c48a70bbb119a6b5e58b2ba1bb044d5e8747c
alinaaldea <43847920+alinaaldea@users.noreply.github.com>
2018-12-07 17:52:18 +0100

    Revert "csp"
    This reverts commit 15e82ca9815da7fa09bea4c4807539854014f446.

--- index.js
+++ index.js
@@ -7,48 +7,18 @@ var app = express();
 var helmet = require("helmet");
 var fs = require("fs");
 var path = require("path");
-const csp = require("helmet-csp");
 
 app.use(express.static("res"));
 
 app.use(helmet());
 
 app.use(
-  csp({
-    // Specify directives as normal.
+  //Helmet’s csp module helps set Content Security Policies.
+  helmet.contentSecurityPolicy({
     directives: {
-      defaultSrc: ["'self'", "default.com"],
-      scriptSrc: ["'self'", "'unsafe-inline'"],
-      styleSrc: ["style.com"],
-      fontSrc: ["'self'", "fonts.com"],
-      imgSrc: ["img.com", "data:"],
-      sandbox: ["allow-forms", "allow-scripts"],
-      reportUri: "/report-violation",
-      objectSrc: ["'none'"],
-      upgradeInsecureRequests: true,
-      workerSrc: false // This is not set.
-    },
-
-    // This module will detect common mistakes in your directives and throw errors
-    // if it finds any. To disable this, enable "loose mode".
-    loose: false,
-
-    // Set to true if you only want browsers to report errors, not block them.
-    // You may also set this to a function(req, res) in order to decide dynamically
-    // whether to use reportOnly mode, e.g., to allow for a dynamic kill switch.
-    reportOnly: false,
-
-    // Set to true if you want to blindly set all headers: Content-Security-Policy,
-    // X-WebKit-CSP, and X-Content-Security-Policy.
-    setAllHeaders: true,
-
-    // Set to true if you want to disable CSP on Android where it can be buggy.
-    disableAndroid: false,
-
-    // Set to false if you want to completely disable any user-agent sniffing.
-    // This may make the headers less compatible but it will be much faster.
-    // This defaults to `true`.
-    browserSniff: true
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'", "maxcdn.bootstrapcdn.com"]
+    }
   })
 );
 
